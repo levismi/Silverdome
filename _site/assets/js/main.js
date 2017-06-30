6297726c7040c5cca04fcf8cdf5b4b8e90b4310a
levismi <levismi@gmail.com>
2017-06-29 20:05:17 -0500

    updates

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,58 +1,54 @@
 $(document).ready(function(){
 
-    // ScrollMagic Init
-    var controller = new ScrollMagic.Controller();
+  // ScrollMagic Init
+  var controller = new ScrollMagic.Controller();
 
-    $('.castCrew').each(function(){
-
-      // Build Scene
-      var fadeScene1 = new ScrollMagic.Scene({
-        triggerElement: this,
-        triggerHook: 0.3,
-        offset: -90,
-      })
-      .setClassToggle(this, 'fade-in') //add fade-in class
-      .addIndicators()
-      .addTo(controller);
-
-    });
-
-
-    var pinScene = new ScrollMagic.Scene({
-      triggerElement: '.cast_glenn',
-      triggerHook:0,
-      duration: '100%'
+  // Build Scene
+  $('.castCrew').each(function(){
+    var fadeScene1 = new ScrollMagic.Scene({
+      triggerElement: this,
+      triggerHook: 0,
+      offset: -90
     })
-    .setPin('.cast_glenn', {pushFollowers: false})
+    .setClassToggle(this, 'fade-in') //add fade-in class
+    .addIndicators()
     .addTo(controller);
+  });
 
+  // Pin Scenes
+  var pinScene = new ScrollMagic.Scene({
+    triggerElement: '.cast_glenn',
+    triggerHook:0,
+    duration: '100%'
+  })
+  .setPin('.cast_glenn', {pushFollowers: false})
+  .addTo(controller);
 
-    var pinScene = new ScrollMagic.Scene({
-      triggerElement: '.cast_carmen',
-      triggerHook:0,
-      duration: '100%'
-    })
-    .setPin('.cast_carmen', {pushFollowers: false})
-    .addTo(controller);
+  var pinScene = new ScrollMagic.Scene({
+    triggerElement: '.cast_carmen',
+    triggerHook:0,
+    duration: '100%'
+  })
+  .setPin('.cast_carmen', {pushFollowers: false})
+  .addTo(controller);
 
+  var pinScene = new ScrollMagic.Scene({
+    triggerElement: '.cast_francisco',
+    triggerHook:0,
+    duration: '100%'
+  })
+  .setPin('.cast_francisco', {pushFollowers: false})
+  .addTo(controller);
 
-    var pinScene = new ScrollMagic.Scene({
-      triggerElement: '.cast_francisco',
-      triggerHook:0,
-      duration: '100%'
-    })
-    .setPin('.cast_francisco', {pushFollowers: false})
-    .addTo(controller);
+  var pinScene = new ScrollMagic.Scene({
+    triggerElement: '.crew',
+    triggerHook:0.,
+    duration: '100%'
+  })
+  .setPin('.crew', {pushFollowers: false})
+  .addTo(controller);
 
-
-    var pinScene = new ScrollMagic.Scene({
-      triggerElement: '.crew',
-      triggerHook:0,
-      duration: '100%'
-    })
-    .setPin('.crew', {pushFollowers: false})
-    .addTo(controller);
-
+  // Parallax
 
 
 
